e92e87f1f7abfd1b6e41c344b44c07d94fb6b2c8
Katyanna Moura <amelie.kn@gmail.com>
2019-04-02 13:45:25 +0200

    Put info in variables before returning it

---
+++ watchlist.py
@@ -0,0 +1,47 @@
+def movies():
+    listed_movies = [{
+        "title": "Roma",
+        "genre": "Drama",
+        "broadcaster": "Netflix",
+    },
+    {
+        "title": "The boy who harnessed the wind",
+        "genre": "Drama",
+        "broadcaster": "Netflix",
+    },
+    {
+        "title": "The Dirt",
+        "genre": "Doc/Comedy",
+        "broadcaster": "Netflix",
+    }]
+
+    return listed_movies
+
+def tvshows():
+    listed_tvshows = [{
+        "title": "Samantha",
+        "genre": "Comedy",
+        "broadcaster": "Netflix",
+    },
+    {
+        "title": "Marvelous Mrs. Masel",
+        "genre": "Comedy",
+        "broadcaster": "Amazon Prime",
+    },
+    {
+        "title": "Most beautiful thing",
+        "genre": "Drama",
+        "broadcaster": "Netflix",
+    },
+    {
+        "title": "Workin' moms",
+        "genre": "Comedy",
+        "broadcaster": "Netflix",
+    },
+    {
+        "title": "This is Us",
+        "genre": "Drama",
+        "broadcaster": "Amazon Prime",
+    }]
+
+    return listed_tvshows
